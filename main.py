9061c8ca5461485701f7ab1a31fec3228fe6f2ea
twiggsk <twiggsk@berea.edu>
2022-09-06 16:09:29 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+# Chinese New Year code to find your animal
+
+yearborn = (int(input("What year were you born?")))
+
+if yearborn == (2003): #or (1991) or (2015):
+    print("You're a Goat!")
+
+elif yearborn == (2004): #or (1992) or (2016):
+    print("You're a Monkey!")
+
+elif yearborn == (2005): #or (1993) or (2017):
+    print("You're a Rooster!")
+
+elif yearborn == (2006): # or (1994) or (2018):
+    print("You're a Dog!")
+
+elif yearborn == (2007): # or (1995) or (2019):
+    print("You're a Pig!")
+
+elif yearborn == (2008): # or (1996) or (2020):
+    print("You're a Rat!")
+
+elif yearborn == (2009): # or (1997) or (2021):
+    print("You're an Ox!")
+
+elif yearborn == (2010): # or (1998) or (2022):
+    print("You're a Tiger!")
+
+elif yearborn == (2011): # or (1999) or (2023):
+    print("You're a Rabbit!")
+
+elif yearborn == (2012): # or (2000) or (2024):
+    print("You're a Dragon!")
+
+elif yearborn == (2013): # or (2001) or (2025):
+    print("You're a Snake!")
+
+elif yearborn == (2014): # or (2002) or (2026):
+    print("You're a Horse!")
+
+
+
+
+
